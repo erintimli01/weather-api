11245d17e99d95900edc2690b767e0991a7eb634
Jacob Maier <jacobamaier@gmail.com>
2023-02-06 15:51:29 -0800

    Update separation of logic.
    Co-authored-by: Erin Timlin <erintimlin@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,47 +1,42 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+import { WeatherService } from './../src/js/main';
 
 // Business Logic
 
-function getWeather(city) {
-  let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-
-  request.addEventListener("loadend", function() {
-    const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      printElements(response, city);
-    } else {
-      printError(this, response, city);
-    }
-  });
-
-  request.open("GET", url, true);
-  request.send();
-}
+// function getWeather(city) {
+//   let promise = WeatherService.getWeather(city);
+//   promise.then(function(weatherDataArray) {
+//     printElements(weatherDataArray);
+//   }, function(errorArray) {
+//     printError(errorArray);
+//   });
+// }
 
 // UI Logic
-
-function printElements(apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%. 
-  The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
-}
-
-function printError(request, apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
-}
 
 function handleFormSubmission(event) {
   event.preventDefault();
   const city = document.querySelector('#location').value;
   document.querySelector('#location').value = null;
-  getWeather(city);
+  let promise = WeatherService.getWeather(city);
+  promise.then(function(weatherDataArray) {
+    printElements(weatherDataArray);
+  }, function(errorArray) {
+    printError(errorArray);
+  });
 }
 
-window.addEventListener("load", function() {
-  document.querySelector('form').addEventListener("submit", handleFormSubmission);
-});
-window.addEventListener("load", function() {
-  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+function printElements(data) {
+  document.querySelector("#showResponse").innerText = `The humidity in ${data[1]} is ${data[0].main.humidity}%. The temperature in Kelvins is ${data[0].main.temp} degrees.`;
+}
+
+function printError(error) {
+  document.querySelector("#showResponse").innerText = `There was an error accessing the weather data for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
+}
+
+
+window.addEventListener("load", function () {
+  document.querySelector("form").addEventListener("submit", handleFormSubmission);
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,18 @@
-// Business Logic
-
-// Export example
-// export default function Example()
-
+export default class WeatherService {
+  static getWeather(city) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+      request.addEventListener('loadend', function() {
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve([response, city]);
+        } else {
+          reject([this, response, city]);
+        }
+      });
+      request.responseXML("GET", url, true);
+      request.send();
+    });
+  }
+}
